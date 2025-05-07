7c418029222b78bc9ef2cd0a47aa7c80755b7137
Maksim N <digitalcrab@users.noreply.github.com>
2025-05-07 16:45:32 +0200

    Update Group Anagrams solution

--- leetcode-solutions/group-anagrams/main.go
+++ leetcode-solutions/group-anagrams/main.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"maps"
-	"slices"
 )
 
 // groupAnagrams returns anagrams grouped together.
-// Time Complexity: N is number of strings, M is the longest string, O(N * M log M) as the largest operation
+// Time Complexity: N is number of strings, M is the longest string, O(N * M) as the largest operation
 // Space Complexity: O(N * M) as we still create the group that potentially consists of all strings individually
 func groupAnagrams(strs []string) [][]string {
-	grouping := make(map[string][]string)
+	grouping := make(map[anagramKey][]string)
 	for _, s := range strs { // O(N * M log M), as we call `sortedString` for each string
 		// as anagram is a words that contains the same letters, it would make sense to sort letters in the word
 		// and use it as a grouping key
@@ -28,13 +27,17 @@ func groupAnagrams(strs []string) [][]string {
 	return groups
 }
 
-// strKey gives the sorted string by character back
-// Time Complexity: O(n log(n)), where n the length of the string, based on the std library
-// TODO: this function could be improved if we do not sort the string, but maybe build the array of char = used times
-func strKey(s string) string {
-	sorted := []rune(s)
-	slices.Sort(sorted)
-	return string(sorted)
+// consists of lowercase English letters
+type anagramKey [26]int
+
+// strKey gives the key for the map, that is an array of characters and how many times they were used
+// Time Complexity: O(n), where n the length of the string,
+func strKey(s string) anagramKey {
+	var key anagramKey
+	for _, ch := range s {
+		key[ch-'a']++
+	}
+	return key
 }
 
 func main() {
