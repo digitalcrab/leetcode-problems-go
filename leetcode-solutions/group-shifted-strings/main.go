948486b2f0b818b1912b71d83b37fb080d2dabc4
Maksim N <digitalcrab@users.noreply.github.com>
2025-05-19 12:45:22 +0200

    Update Group Shifted Strings with example explanation

--- leetcode-solutions/group-shifted-strings/main.go
+++ leetcode-solutions/group-shifted-strings/main.go
@@ -3,7 +3,17 @@ package main
 // build a key, as a combination of distances between chars
 func createKey(s string) string {
 	var key string
+
 	for i := 1; i < len(s); i++ {
+		// Step-by-step explanation for "az" and "ba":
+		// For "az":
+		// i = 1: diff = (26 + ('z' - 'a') - ('a' - 'a')) % 26 = (26 + 25 - 0) % 26 = 25
+		// key += string(25 + 'a') = key += 'z'
+
+		// For "ba":
+		// i = 1: diff = (26 + ('a' - 'a') - ('b' - 'a')) % 26 = (26 + 0 - 1) % 26 = 25
+		// key += string(25 + 'a') = key += 'z'
+
 		diff := (26 + s[i] - 'a' - s[i-1] - 'a') % 26
 		key += string(diff + 'a') // + 'a' makes it just human-readable bringing back to the a-z range
 	}
